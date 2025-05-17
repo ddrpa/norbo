369f41db1a6d8331dc8a487c0a82c0331446d5d6
yufan <yufan@live.com>
2025-05-17 14:01:35 +0800

    feat 支持 MappedTypes

--- src/main/java/cc/ddrpa/dorian/norbo/mabtisplus/processor/MPTypeHandlerProcessor.java
+++ src/main/java/cc/ddrpa/dorian/norbo/mabtisplus/processor/MPTypeHandlerProcessor.java
@@ -79,84 +79,53 @@ public class MPTypeHandlerProcessor extends AbstractProcessor {
             boolean isElementParameterized = annotatedElement.asType() instanceof DeclaredType d &&
                 !d.getTypeArguments().isEmpty();
 
-            TypeSpec.Builder typeBuilder = TypeSpec.classBuilder(className)
-                .addAnnotation(
+            TypeSpec.Builder typeBuilder = TypeSpec.classBuilder(className);
+            // @MappedTypes
+            if (isElementParameterized) {
+                DeclaredType declaredType = (DeclaredType) annotatedElement.asType();
+                TypeElement rawType = (TypeElement) declaredType.asElement();
+                typeBuilder.addAnnotation(
                     AnnotationSpec.builder(
-                            ClassName.get("org.apache.ibatis.type", "MappedJdbcTypes"))
-                        .addMember("value", "{$T.$L, $T.$L}",
+                            ClassName.get("org.apache.ibatis.type", "MappedTypes"))
+                        .addMember("value", "$T.class", ClassName.get(rawType))
+                        .build());
+            } else {
+                typeBuilder.addAnnotation(
+                    AnnotationSpec.builder(
+                            ClassName.get("org.apache.ibatis.type", "MappedTypes"))
+                        .addMember("value", "$T.class", annotatedElementType)
+                        .build());
+            }
+
+            typeBuilder.addAnnotation(
+                    AnnotationSpec.builder(ClassName.get("org.apache.ibatis.type", "MappedJdbcTypes"))
+                        .addMember("value",
+                            "{$T.$L, $T.$L}",
                             ClassName.get("org.apache.ibatis.type", "JdbcType"), "VARCHAR",
                             ClassName.get("org.apache.ibatis.type", "JdbcType"), "LONGVARCHAR")
                         .build())
                 .addModifiers(PUBLIC)
-                .superclass(ParameterizedTypeName.get(
-                    ClassName.get("com.baomidou.mybatisplus.extension.handlers",
-                        "AbstractJsonTypeHandler"),
-                    annotatedElementType));
+                .superclass(
+                    ParameterizedTypeName.get(
+                        ClassName.get("com.baomidou.mybatisplus.extension.handlers",
+                            "AbstractJsonTypeHandler"),
+                        annotatedElementType));
 
-            // NEED_CHECK 应当为每个 typehandler 创建自己的 objectMapper 实例还是实现依赖注入？
+            // NEED_CHECK 应当为每个 typeHandler 创建自己的 objectMapper 实例还是实现依赖注入？
             // 添加 ObjectMapper
             typeBuilder.addField(
                 FieldSpec.builder(ClassName.get("com.fasterxml.jackson.databind", "ObjectMapper"),
                         "mapper")
                     .addModifiers(Modifier.PRIVATE, Modifier.FINAL)
                     .initializer("new $T()",
-                        ClassName.get("com.fasterxml.jackson.databind", "ObjectMapper")).build());
-
-            // 在构造函数中配置 ObjectMapper
-            MethodSpec constructor = MethodSpec.constructorBuilder()
-                .addModifiers(Modifier.PUBLIC)
-                .addParameter(Class.class, "type")
-                .addStatement("super(type)")
-                // ObjectMapper 配置
-                .addStatement("mapper.registerModule(new $T())",
-                    ClassName.get("com.fasterxml.jackson.datatype.jsr310", "JavaTimeModule"))
-                .addStatement("mapper.disable($T.$L)",
-                    ClassName.get("com.fasterxml.jackson.databind", "SerializationFeature"),
-                    "WRITE_DATES_AS_TIMESTAMPS")
-                .addStatement("mapper.registerModule(new $T())",
-                    ClassName.get("com.fasterxml.jackson.datatype.jdk8", "Jdk8Module"))
-                .addStatement("mapper.disable($T.$L)",
-                    ClassName.get("com.fasterxml.jackson.databind", "SerializationFeature"),
-                    "FAIL_ON_EMPTY_BEANS")
-                .addStatement("mapper.disable($T.$L)",
-                    ClassName.get("com.fasterxml.jackson.databind", "DeserializationFeature"),
-                    "FAIL_ON_UNKNOWN_PROPERTIES")
-                .addStatement("mapper.disable($T.$L)",
-                    ClassName.get("com.fasterxml.jackson.databind", "DeserializationFeature"),
-                    "FAIL_ON_NULL_FOR_PRIMITIVES")
-                .addStatement("mapper.enable($T.$L)",
-                    ClassName.get("com.fasterxml.jackson.databind", "DeserializationFeature"),
-                    "ACCEPT_EMPTY_STRING_AS_NULL_OBJECT")
-                .addStatement("mapper.disable($T.$L)",
-                    ClassName.get("com.fasterxml.jackson.databind", "DeserializationFeature"),
-                    "FAIL_ON_NUMBERS_FOR_ENUMS")
-                .addStatement(
-                    "mapper.configOverride($T.class).setSetterInfo($T.forContentNulls($T.AS_EMPTY))",
-                    List.class,
-                    ClassName.get("com.fasterxml.jackson.annotation", "JsonSetter", "Value"),
-                    ClassName.get("com.fasterxml.jackson.annotation", "Nulls"))
-                .addStatement(
-                    "mapper.configOverride($T.class).setSetterInfo($T.forValueNulls($T.AS_EMPTY))",
-                    List.class,
-                    ClassName.get("com.fasterxml.jackson.annotation", "JsonSetter", "Value"),
-                    ClassName.get("com.fasterxml.jackson.annotation", "Nulls"))
-                .build();
-
-            // 构建 toJson 方法
-            MethodSpec toJsonMethod = MethodSpec.methodBuilder("toJson")
-                .addAnnotation(ClassName.get("lombok", "SneakyThrows"))
-                .addAnnotation(Override.class)
-                .addModifiers(Modifier.PUBLIC)
-                .addParameter(annotatedElementType, "obj")
-                .addStatement("return mapper.writeValueAsString(obj)")
-                .returns(String.class)
-                .build();
+                        ClassName.get("com.fasterxml.jackson.databind", "ObjectMapper"))
+                    .build());
 
             // 添加方法到类
             typeBuilder
-                .addMethod(constructor)
+                .addMethod(constructorMethod())
                 .addMethod(parseMethod(annotatedElementType, isElementParameterized))
-                .addMethod(toJsonMethod);
+                .addMethod(toJsonMethod(annotatedElementType));
 
             JavaFile file = JavaFile.builder(className.packageName(), typeBuilder.build()).build();
             try {
@@ -231,12 +200,51 @@ public class MPTypeHandlerProcessor extends AbstractProcessor {
     }
 
     /**
-     * 构建 parse 方法
+     * 创建构造函数，在构造函数中配置 ObjectMapper
      *
-     * @param annotatedElementType
-     * @param isElementParameterized
      * @return
      */
+    protected MethodSpec constructorMethod() {
+        return MethodSpec.constructorBuilder()
+            .addModifiers(Modifier.PUBLIC)
+            .addParameter(Class.class, "type")
+            .addStatement("super(type)")
+            // ObjectMapper 配置
+            .addStatement("mapper.registerModule(new $T())",
+                ClassName.get("com.fasterxml.jackson.datatype.jsr310", "JavaTimeModule"))
+            .addStatement("mapper.disable($T.$L)",
+                ClassName.get("com.fasterxml.jackson.databind", "SerializationFeature"),
+                "WRITE_DATES_AS_TIMESTAMPS")
+            .addStatement("mapper.registerModule(new $T())",
+                ClassName.get("com.fasterxml.jackson.datatype.jdk8", "Jdk8Module"))
+            .addStatement("mapper.disable($T.$L)",
+                ClassName.get("com.fasterxml.jackson.databind", "SerializationFeature"),
+                "FAIL_ON_EMPTY_BEANS")
+            .addStatement("mapper.disable($T.$L)",
+                ClassName.get("com.fasterxml.jackson.databind", "DeserializationFeature"),
+                "FAIL_ON_UNKNOWN_PROPERTIES")
+            .addStatement("mapper.disable($T.$L)",
+                ClassName.get("com.fasterxml.jackson.databind", "DeserializationFeature"),
+                "FAIL_ON_NULL_FOR_PRIMITIVES")
+            .addStatement("mapper.enable($T.$L)",
+                ClassName.get("com.fasterxml.jackson.databind", "DeserializationFeature"),
+                "ACCEPT_EMPTY_STRING_AS_NULL_OBJECT")
+            .addStatement("mapper.disable($T.$L)",
+                ClassName.get("com.fasterxml.jackson.databind", "DeserializationFeature"),
+                "FAIL_ON_NUMBERS_FOR_ENUMS")
+            .addStatement(
+                "mapper.configOverride($T.class).setSetterInfo($T.forContentNulls($T.AS_EMPTY))",
+                List.class,
+                ClassName.get("com.fasterxml.jackson.annotation", "JsonSetter", "Value"),
+                ClassName.get("com.fasterxml.jackson.annotation", "Nulls"))
+            .addStatement(
+                "mapper.configOverride($T.class).setSetterInfo($T.forValueNulls($T.AS_EMPTY))",
+                List.class,
+                ClassName.get("com.fasterxml.jackson.annotation", "JsonSetter", "Value"),
+                ClassName.get("com.fasterxml.jackson.annotation", "Nulls"))
+            .build();
+    }
+
     protected MethodSpec parseMethod(TypeName annotatedElementType,
         boolean isElementParameterized) {
         MethodSpec.Builder methodBuilder = MethodSpec.methodBuilder("parse")
@@ -254,5 +262,17 @@ public class MPTypeHandlerProcessor extends AbstractProcessor {
                 annotatedElementType);
         }
         return methodBuilder.build();
+    }
+
+    protected MethodSpec toJsonMethod(TypeName annotatedElementType) {
+        // 构建 toJson 方法
+        return MethodSpec.methodBuilder("toJson")
+            .addAnnotation(ClassName.get("lombok", "SneakyThrows"))
+            .addAnnotation(Override.class)
+            .addModifiers(Modifier.PUBLIC)
+            .addParameter(annotatedElementType, "obj")
+            .addStatement("return mapper.writeValueAsString(obj)")
+            .returns(String.class)
+            .build();
     }
 }
